235935c9f81283f885bdc338000f41384ab1c11a
Sunwoo Lee <slee20@stanford.edu>
2023-02-25 12:13:05 -0800

    light mode now

--- pages/login.tsx
+++ pages/login.tsx
@@ -9,37 +9,54 @@ const Home = () => {
   const session = useSession();
   const supabase = useSupabaseClient();
 
-  const customTheme = {
-    // You can also add more theme variations with different names.
-    pink: {
-      colors: {
-        brandButtonText: "white",
-        defaultButtonBackground: "#FF0000",
-        defaultButtonBackgroundHover: "#2e2e2e",
-        //..
-      },
-    },
-    default: {
-      colors: {
-        brandButtonText: "white",
-        defaultButtonBackground: "#1e1e1e",
-        defaultButtonBackgroundHover: "#2e2e2e",
-        //..
-      },
-    },
-  };
   return (
     <div style={{ padding: "0 0 0 0" }}>
       {!session ? (
         <div className={styles.container}>
           <div className={styles.left}>
             <div className={styles.auth}>
-              <div>Quest</div>
-              <Auth
+              <div>Login to Quest</div>
+              {/* <Auth
                 providers={["google", "github", "linkedin"]}
                 supabaseClient={supabase}
                 appearance={{ theme: ThemeSupa }}
                 theme="dark"
+              /> */}
+              <Auth
+                providers={["google", "github", "linkedin"]}
+                supabaseClient={supabase}
+                appearance={{
+                  theme: ThemeSupa,
+                  variables: {
+                    default: {
+                      colors: {
+                        brand: "#ED03F4",
+                        brandAccent: "#ED03F4",
+                        defaultButtonBackground: "ED03F4",
+                        defaultButtonBackgroundHover: "#A400A9",
+                        defaultButtonBorder: "lightgray",
+                        defaultButtonText: "gray",
+                        dividerBackground: "#eaeaea",
+                        inputBackground: "transparent",
+                        inputBorder: "lightgray",
+                        inputBorderHover: "gray",
+                        inputBorderFocus: "gray",
+                        inputText: "black",
+                        inputLabelText: "gray",
+                        inputPlaceholder: "darkgray",
+                        messageText: "gray",
+                        anchorTextColor: "gray",
+                        anchorTextHoverColor: "darkgray",
+                      },
+                      fonts: {
+                        bodyFontFamily: "avenir, helvetica, sans-serif",
+                        buttonFontFamily: "avenir, helvetica, sans-serif",
+                        inputFontFamily: "avenir, helvetica, sans-serif",
+                        labelFontFamily: "avenir, helvetica, sans-serif",
+                      },
+                    },
+                  },
+                }}
               />
             </div>
           </div>
